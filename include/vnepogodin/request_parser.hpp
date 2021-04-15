bae62e54304240202348b4b876056c4bd0ea8296
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2021-04-16 02:39:15 +0400

    Add Licence

--- include/vnepogodin/request_parser.hpp
+++ include/vnepogodin/request_parser.hpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2021 Vladislav Nepogodin
+ *
+ * This file is part of SportTech overlay project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #ifndef REQUEST_PARSER_HPP_
 #define REQUEST_PARSER_HPP_
 
@@ -27,8 +47,8 @@ class request_parser {
     // input has been consumed.
     template<typename InputIterator>
     std::tuple<boost::tribool, InputIterator> parse(request& req,
-                                             InputIterator begin,
-                                             InputIterator end) {
+                                                    InputIterator begin,
+                                                    InputIterator end) {
         while (begin != end) {
             boost::tribool result = consume(req, *begin++);
             if (result || !result)
